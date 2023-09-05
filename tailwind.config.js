83952c257299126837551979f5464c17d4101e0b
‘fazdajulioarzika’ <zotenk473@gmail.com>
2023-09-05 09:07:45 +0700

    update bug src gambar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
   theme: {
     container: {
       center: true,
-      padding: '8px',
+      padding: {
+        DEFAULT: '64px',
+      },
     },
     extend: {
       colors: {
